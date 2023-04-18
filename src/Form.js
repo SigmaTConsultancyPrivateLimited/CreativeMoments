e0d862e604860916a9289e0f6474d33b65e3f83c
SigmaT Consultancy Private Limited <101176337+SigmaTConsultancyPrivateLimited@users.noreply.github.com>
2023-04-18 16:08:41 +0530

    Add files via upload

--- src/Form.js
+++ src/Form.js
@@ -13,34 +13,33 @@ export default function Form() {
         <div className="register">
             <div className="col-1">
                 <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-                    <h2 className="title">Request</h2>
-                    <p> We have sent the Request</p>
+                    <h2 className="title">Shipping Details</h2>
 
+                    <label className="firstname">FirstName: </label>
+                    <input type="text" placeholder='John' />
+
+                    <label className="lastname">LastName: </label>
+                    <input type="text" placeholder='S' />
+                    <label className="address">Address: </label>
+                    <input type="text" placeholder='Uppsala, Swedan' />
+                    <label className="postalcode">Postal Code</label>
+                    <input type="text" placeholder='123 987' />
                     <button className='btn'>Submit</button>
                 </form>
                 
                 <h2>My Cart</h2>
                 <div className="mycart">
                     <img className="product-img" src={image} alt="" />
-                    <h5 className="product-title">Little Adventure Card</h5>
-                    <h5 className="price">549.00kr</h5>
+                    <h5 className="product-title">"Little Adventure Card"</h5>
+                    <h5 className="price"> "549.00kr"</h5>
                     
                     <img className="product-img1" src={image} alt="" />
-                    <h5 className="prod-title1">Stora klatterkortet</h5>
-                    <h5 className="price1">1,549.00kr</h5>
+                    <h5 className="product-title1">"Stora klatterkortet"</h5>
+                    <h5 className="price1"> "1,549.00kr"</h5>
                     <h5 className="total">Total:  2100.00kr </h5>
                     <button className="btn-success">Edit Cart</button>
-
                 </div>
                 
-                
-                <div className='email-box'>
-                    <h4 class="align"> Sign Up to our Newsletter</h4>
-                    <div className="form-group">
-                        <input name="Your Email" type="email" className="form-control" placeholder="Your Email" />
-                    </div>
-                    <button style={{ backgroundColor:"orange"}}> Sign Up</button> 
-                </div>
 
             </div>
         </div>
